e62ffcf2102f07ec30db126c78a099225ea0070c
lubna <lubnalmaaweed@gmail.com>
2021-01-19 20:50:18 +0200

    my answers are uploaded

---
+++ index.php
@@ -0,0 +1,202 @@
+<?php
+
+                        //Question 1 //
+<!DOCTYPE html>
+
+<head>
+	<title>PHP - Calculate Electricity Bill</title>
+</head>
+
+<?php
+$result_str = $result = '';
+if (isset($_POST['unit-submit'])) {
+    $units = $_POST['units'];
+    if (!empty($units)) {
+        $result = calculate_bill($units);
+        $result_str = 'Total amount of ' . $units . ' - ' . $result;
+    }
+}
+/**
+ * To calculate electricity bill as per unit cost
+ */
+function calculate_bill($units) {
+    $unit_cost_first = 3.50;
+    $unit_cost_second = 4.00;
+    $unit_cost_third = 5.20;
+    $unit_cost_fourth = 6.50;
+
+    if($units <= 50) {
+        $bill = $units * $unit_cost_first;
+    }
+    else if($units > 50 && $units <= 100) {
+        $temp = 50 * $unit_cost_first;
+        $remaining_units = $units - 50;
+        $bill = $temp + ($remaining_units * $unit_cost_second);
+    }
+    else if($units > 100 && $units <= 200) {
+        $temp = (50 * 3.5) + (100 * $unit_cost_second);
+        $remaining_units = $units - 150;
+        $bill = $temp + ($remaining_units * $unit_cost_third);
+    }
+    else {
+        $temp = (50 * 3.5) + (100 * $unit_cost_second) + (100 * $unit_cost_third);
+        $remaining_units = $units - 250;
+        $bill = $temp + ($remaining_units * $unit_cost_fourth);
+    }
+    return number_format((float)$bill, 2, '.', '');
+}
+
+?>
+
+<body>
+	<div id="page-wrap">
+		<h1>Php - Calculate Electricity Bill</h1>
+
+		<form action="" method="post" id="quiz-form">
+            	<input type="number" name="units" id="units" placeholder="Please enter no. of Units" />
+            	<input type="submit" name="unit-submit" id="unit-submit" value="Submit" />
+		</form>
+
+		<div>
+		    <?php echo '<br />' . $result_str; ?>
+		</div>
+	</div>
+</body>
+</html>
+
+                        //Question 2 //
+
+<!DOCTYPE html>
+
+<head>
+	<title>Simple Calculator Program in PHP - Tutorials Class</title>
+</head>
+
+<?php
+$first_num = $_POST['first_num'];
+$second_num = $_POST['second_num'];
+$operator = $_POST['operator'];
+$result = '';
+if (is_numeric($first_num) && is_numeric($second_num)) {
+    switch ($operator) {
+        case "Add":
+           $result = $first_num + $second_num;
+            break;
+        case "Subtract":
+           $result = $first_num - $second_num;
+            break;
+        case "Multiply":
+            $result = $first_num * $second_num;
+            break;
+        case "Divide":
+            $result = $first_num / $second_num;
+    }
+}
+
+?>
+
+<body>
+    <div id="page-wrap">
+	<h1>PHP - Simple Calculator Program</h1>
+	  <form action="" method="post" id="quiz-form">
+            <p>
+                <input type="number" name="first_num" id="first_num" required="required" value="<?php echo $first_num; ?>" /> <b>First Number</b>
+            </p>
+            <p>
+                <input type="number" name="second_num" id="second_num" required="required" value="<?php echo $second_num; ?>" /> <b>Second Number</b>
+            </p>
+            <p>
+                <input readonly="readonly" name="result" value="<?php echo $result; ?>"> <b>Result</b>
+            </p>
+            <input type="submit" name="operator" value="Add" />
+            <input type="submit" name="operator" value="Subtract" />
+            <input type="submit" name="operator" value="Multiply" />
+            <input type="submit" name="operator" value="Divide" />
+	  </form>
+    </div>
+</body>
+</html>
+
+<!DOCTYPE html>
+
+<head>
+	<title>Simple Calculator Program in PHP - Tutorials Class</title>
+</head>
+
+<?php
+$first_num = $_POST['first_num'];
+$second_num = $_POST['second_num'];
+$operator = $_POST['operator'];
+$result = '';
+if (is_numeric($first_num) && is_numeric($second_num)) {
+    switch ($operator) {
+        case "Add":
+           $result = $first_num + $second_num;
+            break;
+        case "Subtract":
+           $result = $first_num - $second_num;
+            break;
+        case "Multiply":
+            $result = $first_num * $second_num;
+            break;
+        case "Divide":
+            $result = $first_num / $second_num;
+    }
+}
+
+?>
+
+<body>
+    <div id="page-wrap">
+	<h1>PHP - Simple Calculator Program</h1>
+	  <form action="" method="post" id="quiz-form">
+            <p>
+                <input type="number" name="first_num" id="first_num" required="required" value="<?php echo $first_num; ?>" /> <b>First Number</b>
+            </p>
+            <p>
+                <input type="number" name="second_num" id="second_num" required="required" value="<?php echo $second_num; ?>" /> <b>Second Number</b>
+            </p>
+            <p>
+                <input readonly="readonly" name="result" value="<?php echo $result; ?>"> <b>Result</b>
+            </p>
+            <input type="submit" name="operator" value="Add" />
+            <input type="submit" name="operator" value="Subtract" />
+            <input type="submit" name="operator" value="Multiply" />
+            <input type="submit" name="operator" value="Divide" />
+	  </form>
+    </div>
+</body>
+</html>
+
+                        //Question 3 //
+
+
+function check_vote() //function has been declared
+{
+    $name = "Rakesh";
+    $age = 19;
+    if ($age >= 18) {
+        echo $name . ", you Are Eligible For Vote";
+    } else {
+        echo $name . ", you are not eligible for vote. ";
+    }
+}
+check_vote(); //function has been called
+
+                        //Question 4 //
+
+
+$number = 38; 
+if ($number > 0)
+{
+    echo $number . " is a positive number";
+} else if ($number < 0) 
+{
+    echo $number . " is a negative number ";
+} else if ($number == 0) 
+{
+    echo "You have entered zero";
+} else {
+    echo " please enter a numeric value";
+
+?>
